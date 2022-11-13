b34e8450beff12f74e0f5e56a4342ecad044bddf
Faterek <seweryn.fater@protonmail.com>
2022-11-13 21:16:06 +0100

    first commit

---
+++ src/routes/[...404].jsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <main class="text-center text-white p-[0.1rem]">
+      <h1 class="text-6xl font-thin uppercase my-16">
+        Page not Found
+      </h1>
+    </main>
+  );
+}
---
+++ src/routes/index.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main class="text-center text-white p-[0.1rem]">
+      <h1 class="text-6xl font-thin uppercase my-16">
+        Hello world!
+      </h1>
+    </main>
+  );
+}
