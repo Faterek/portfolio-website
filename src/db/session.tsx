4300cc6efc3a5e5c971cd4788106059a68133cc0
Faterek <seweryn.fater@protonmail.com>
2022-11-15 15:53:44 +0100

    add api 404 and finish db/session.tsx

--- src/db/session.tsx
+++ src/db/session.tsx
@@ -7,7 +7,7 @@ type LoginForm = {
 };
 
 export async function login({ username, passwd }: LoginForm) {
-  const user = await db.query("SELECT id, username, password FROM users WHERE username = $username;", { username });
+  const user = await db.query("SELECT * FROM users WHERE username = $username;", { username });
 
   if (user[0].result == false) return null;
   const result = user[0].result[0]
@@ -22,7 +22,7 @@ const storage = createCookieSessionStorage({
   cookie: {
     name: "FaterSite_session",
     secure: true,
-    secrets: ["rozentuzjazmowana-konstantynopolitanczykowianeczka-je-ciastka"],
+    secrets: [import.meta.env.SESSION_SECRET],
     sameSite: "lax",
     path: "/",
     maxAge: 60 * 60 * 24 * 30, // second * minutes * hours * days 30 days
@@ -43,11 +43,38 @@ export async function getUserId(request: Request) {
 
 export async function logout(request: Request) {
   const session = await storage.getSession(request.headers.get("Cookie"));
-  return redirect("/login", {
+  return redirect("/", {
     headers: {
       "Set-Cookie": await storage.destroySession(session)
     }
   });
+}
+
+export async function requireUserId(
+  request: Request,
+  redirectTo: string = new URL(request.url).pathname
+) {
+  const session = await getUserSession(request);
+  const userId = session.get("userId");
+  if (!userId || typeof userId !== "string") {
+    const searchParams = new URLSearchParams([["redirectTo", redirectTo]]);
+    throw redirect(`/login?${searchParams}`);
+  }
+  return userId;
+}
+
+export async function getUser(request: Request) {
+  const userId = await getUserId(request);
+  if (typeof userId !== "string") {
+    return null;
+  }
+
+  try {
+    const user = await db.query("SELECT * FROM users WHERE id = $userId;", { userId })
+    return user;
+  } catch {
+    throw logout(request);
+  }
 }
 
 export async function createUserSession(userId: string, redirectTo: string) {
