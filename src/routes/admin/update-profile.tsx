1d2c398a0549fe0a555231b4d14919c05657d11e
Faterek <seweryn.fater@proton.me>
2022-12-06 15:44:55 +0100

    continue

--- src/routes/admin/update-profile.tsx
+++ src/routes/admin/update-profile.tsx
@@ -17,11 +17,6 @@ export function routeData() {
 export default function UpdateProfile() {
     const user = useRouteData<typeof routeData>();
     const [preview, setPreview] = createSignal("")
-    const [source, setSource] = createSignal((target: HTMLImageElement) => {
-        new Cropper(target)
-        return preview()
-
-    })
     const [updateProfile, { Form }] = createServerAction$(async (form: FormData) => {
         const file = form.get("avatar");
         const displayName = form.get("displayname");
@@ -41,7 +36,7 @@ export default function UpdateProfile() {
                 <input type="text" name="displayname" class="text-black focus:text-black rounded-md p-1"></input><br /><br /><br />
                 <label for="avatar-input">Profile image: </label><br /><br />
                 <div class="rounded-lg w-[50vh] h-[50vh] outline-1 outline outline-[darkgray] outline-offset-2 p-2 mx-auto inline-block align-middle">
-                    <img src={(event) => setSource(event.currentTarget)} class="max-w-[100%] max-h-[100%] w-auto h-auto m-auto block"/>
+                    <img {...new Cropper(this)} id="avatar-img" src={preview()} onchange={(event) => new Cropper(event.currentTarget)} class="max-w-[100%] max-h-[100%] w-auto h-auto m-auto block"/>
                 </div><br /><br />
                 <input name="avatar" type="file" onchange={(event) => setPreview(URL.createObjectURL(event.currentTarget.files[0]))}></input><br /><br /><br />
                 <button type="submit" class="submit-button ml-auto">Update</button>
