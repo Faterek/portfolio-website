a59858af3f51abf74b779e55b8fac5a72ae3b25b
Faterek <seweryn.fater@proton.me>
2022-12-12 12:15:40 +0100

    add opengraph meta tags

--- src/root.tsx
+++ src/root.tsx
@@ -27,6 +27,11 @@ export default function Root() {
                 <Title>Seweryn Fater</Title>
                 <Meta charset='utf-8' />
                 <Meta name='viewport' content='width=device-width, initial-scale=1' />
+                <Meta property='og:type' content='website' />
+                <Meta property='og:title' content='Seweryn Fater portfolio website' />
+                <Meta property='og:description' content='website description' />
+                <Meta property='og:url' content='https://fater.cf' />
+                <Meta property='og:image' content='full thumbnail path' />
             </Head>
             <Body>
                 <Suspense fallback='Loading...'>
